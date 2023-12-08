513b6e7ffeb4326ad3e26ef1f163f973e2de21fa
Olga <Taylakova_Olga@mail.ru>
2023-12-08 15:17:44 +0700

    clean garbage

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -12,7 +12,7 @@ public class CallbackTest {
 
     @Test
     void shouldTestWithValidFirstSurnameSecondName() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Тайлакова Ольга");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
@@ -23,7 +23,7 @@ public class CallbackTest {
 
     @Test
     void shouldTestWithValidFirstNameSecondSurname() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
         form.$("[data-test-id=phone] input").setValue("+79988778899");
@@ -34,129 +34,112 @@ public class CallbackTest {
 
     @Test
     void shouldTestWithhoutName() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        //  String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //   assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldTestWithEmptyPhone() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
+        form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        // String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //  assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 
     @Test
     void shouldTestWithCheckbox() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$(By.className("button")).click();
-        // String textColor = $(By.className("checkbox__text")).getCssValue("color");
-        //assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
     @Test
     void shouldTestWithLatinName() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Olga Tailakova");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        // String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        // assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldTestNumerWithName() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("1111111111");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        // String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //   assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldTestWithNameWithSpecialSymbol() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольг@ Тайлаков@");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        // String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //  assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldTestWithOnlyName() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга");
         form.$("[data-test-id=phone] input").setValue("+79537924254");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        //   String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //  assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldTestWithSpacesInPhoneNumber() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
         form.$("[data-test-id=phone] input").setValue("+7 953 792 42 54");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        //  String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //  assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldTestWithDashInPhoneNumber() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
         form.$("[data-test-id=phone] input").setValue("+7-953-792-42-54");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        //String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //     assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldTestIfPhoneNumberInLatin() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
         SelenideElement form = $(By.className("form"));
         form.$("[data-test-id=name] input").setValue("Ольга Тайлакова");
         form.$("[data-test-id=phone] input").setValue("xxxxxxxxxx");
         form.$("[data-test-id=agreement]").click();
         form.$(By.className("button")).click();
-        $(By.className("input_invalid")).find(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        //  String textColor = $(By.className("input_invalid")).find(".input__sub").getCssValue("color");
-        //   assertEquals("rgba(255, 92, 92, 1)", textColor);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
 
